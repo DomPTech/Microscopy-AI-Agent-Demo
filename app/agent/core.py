f597d5447da7f8342bf65afed5b6dea2b2f6c6b2
DomPTech <domptech2@gmail.com>
2025-12-09 20:39:42 -0500

    feat: Created project skeleton.

---
+++ app/agent/core.py
@@ -0,0 +1,9 @@
+class Agent:
+    def __init__(self):
+        pass
+
+    def chat(self, user_input: str) -> str:
+        """
+        Process user input and return a response.
+        """
+        return "This is a dummy agent response."
